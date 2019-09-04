c6042e358ab2f30207a4b03afa7b6daf81c3a36c
Aldi Nugraha <aldinugrahap16@gmail.com>
2019-09-05 03:33:22 +0700

    add queue-groups

---
+++ queue-groups/publisher.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const NATS = require('nats')
+const nats = NATS.connect()
+
+const interval = 5000
+
+function sendRandomSuhu () {
+  // generate random number beetwen 0 and 99
+  const randomNumber = Math.floor(Math.random() * 100)
+  const topic = 'jawabarat.bandung.suhu'
+  nats.publish(topic, randomNumber.toString())
+  console.log(`[PUBLISHER] Publish ${randomNumber} to ${topic}`)
+}
+
+function onConnectHandler () {
+  console.log(`[PUBLISHER] Connected, ready for publish random suhu every ${interval/1000} Second`)
+}
+
+nats.on('connect', onConnectHandler)
+
+// publish random number every 5 second
+setInterval(sendRandomSuhu, interval)
---
+++ queue-groups/subscriber.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const NATS = require('nats')
+
+const nats = NATS.connect()
+const ID = process.argv[2]
+const TOPIC = 'jawabarat.bandung.suhu'
+
+// check if id filled
+if (typeof ID === 'undefined') {
+  console.error(`ID can't empty.`)
+  process.exit(1)
+}
+
+function onConnectHandler () {
+  console.log(`[SUBSCRIBER-${ID}] (${TOPIC}) Connected, ready receive message.`)
+}
+
+function handleBandungSuhuTopic (msg) {
+  console.log(`[SUBSCRIBER-${ID}] (${TOPIC}) Received data: ${msg}`)
+}
+
+nats.on('connect', onConnectHandler)
+nats.subscribe(TOPIC, {'queue': 'job.workers'} ,handleBandungSuhuTopic)
